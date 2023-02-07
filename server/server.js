fafd02ae541f9a6fab922b517e245f9b692ca6c4
lukavelic <96191586+lukavelic@users.noreply.github.com>
2023-02-07 12:22:41 +0100

    Basic setup

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hey Express')
+})
+
+app.listen(PORT, () => { console.log(`Server started on port: ${PORT}`)})
